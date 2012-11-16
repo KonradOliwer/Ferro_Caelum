8037526ae90d8f9ea243fc0b3f0c99c4404bb329
konrad <kcomrp@gmail.com>
2012-11-16 15:35:34 +0100

    Update o uzgodnienia z ostatniego spotkania

--- hero/models.py
+++ hero/models.py
@@ -6,16 +6,16 @@ class EffectTypes(models.Model):
     name = models.CharField(max_length=50)
 
 class Effect(models.Model):
-    INFINITE = 0
-    
-    last = models.PositiveSmallIntegerField(default=INFINITE)
     type = models.ForeignKey(EffectTypes, blank=True)
+    INFINITE = 0
+    dont_save_after_add = models.BooleanField(default=False)
+    last = models.PositiveSmallIntegerField(default=INFINITE)
     stat = models.ForeignKey(StatsType)
-    formula = models.ForeignKey(AlgebraSignature)
+    formula = models.ForeignKey(Formula)
     additive = models.BooleanField(default=True)
 
 class Condition(models.Model):
-    OPERATOR_CHOICES=(
+    OPERATOR_CHOICES = (
                       (1, '<'),
                       (2, '>'),
                       (3, '='),
@@ -26,48 +26,31 @@ class Condition(models.Model):
     right = models.ForeignKey(StatsType, related_name='right', blank=True)
     # if (left) (operator) value*(rigth or 1) 
     # np. if power > 0.05*hp
-    
 
-class Action(models.Model):
-    SELF = 1
-    ENEMY = 2
-    ALLY = 3
-    
-    TARGET_CHOICES=(
-        (SELF, 'osobisty'),
-        (ENEMY, 'przeciwnik'),
-        (ALLY, 'sojusznik'),
-        )
-    
-    name = models.CharField(max_length=50)
-    auto_activation = models.BooleanField(default=False)
-    auto_activation_conditions = models.ManyToManyField(Condition)
-    only_battle = models.BooleanField(default=True)
-    effects = models.ManyToManyField(Effect)
-    target = models.PositiveSmallIntegerField(choices=TARGET_CHOICES, default=SELF)
 
 class Ability(models.Model):
     name = models.CharField(max_length=50)
-    action = models.ManyToManyField(Action)
     abilities_requirements = models.ManyToManyField('self', symmetrical=False)
     lvl_requirement = models.IntegerField(default=0)
     stats_abilities_requirements = models.ManyToManyField(Condition)
     
 class Fighter(models.Model):
+    name = models.CharField(max_length=50)
+    lvl = models.PositiveSmallIntegerField(default=1)
+    package = models.ManyToManyField(Package)
     stats = models.ManyToManyField(Stat)
     ability = models.ManyToManyField(Ability)    
     
 class Slot(models.Model):
     name = models.CharField(max_length=50)
-
-#Wymaga modyfuikacji:    
+    
 class Item(models.Model):
     type = models.ManyToManyField(Package)
     stats = models.ManyToManyField(Stat)
     abilities = models.ManyToManyField(Ability)
     used_slots = models.ManyToManyField(Slot)
     
-class Enemy(Fighter):
+class NPC(Fighter):
     pass
 
 class BloodLine(models.Model):
@@ -77,8 +60,6 @@ class Profession(models.Model):
     name = models.CharField(max_length=50)
 
 class Hero(Fighter):
-    name = models.CharField(max_length=50)
-    lvl = models.PositiveSmallIntegerField(default=1)
     exp = models.PositiveIntegerField(default=0)
     bloodline = models.ForeignKey(BloodLine)
     profession = models.ForeignKey(Profession)
--- hero/stats_and_abstract.py
+++ hero/stats_and_abstract.py
@@ -1,37 +1,16 @@
 # coding: utf-8
 from django.db import models
 
-class Package(models.Model):
-    name = models.CharField(max_length=50)
-
 class StatsType(models.Model):
     """Dane na temat statystyk i sposobów ich obliczania u szystkich bohaterów"""
     name = models.CharField(max_length=50)
-    packages = models.ManyToManyField(Package)
+	
+class Package(models.Model):
+    name = models.CharField(max_length=50)
+    stats = models.ManyToManyField(StatsType)
 
-class AlgebraSignature(models.Model):
-    """
-    Element wzoru wyliczającego statystykę poboczną
-     
-    Może być wartością statystyki, stałą, operatorem lub znakiem pustym.
-    """
-    OPERATOR_CHOICES=(
-                      (1, '+'),
-                      (2, '-'),
-                      (3, '*'),
-                      (4, '/'),
-                      (5, '^'),
-                      (6, '-'),
-                      (7, 'log'),
-                      (9, 'funkcja wykładnicza'), 
-                      (10, 'liczba z przedziału')
-                    )
-    stat_value = models.ForeignKey(StatsType, blank=True)
-    self_stat_value = models.BooleanField(default=True)
-    constans = models.DecimalField(max_digits=9, decimal_places=3, blank=True)
-    operation = models.IntegerField(choices=OPERATOR_CHOICES, blank=True)
-    signature_right = models.ForeignKey('self', related_name='right', blank=True)
-    signature_left = models.ForeignKey('self', related_name='left', blank=True)
+class Formula(models.Model):
+    text = models.CharField(max_length=100)
 
 class Stat(models.Model):
     """
@@ -41,4 +20,4 @@ class Stat(models.Model):
     additive_change = models.IntegerField(default=0)
     percent_change = models.PositiveIntegerField(default=0)
     base_value = models.IntegerField(default=1)
-    formula = models.ForeignKey(AlgebraSignature, blank=True)
+    formula = models.ForeignKey(Formula, blank=True)
